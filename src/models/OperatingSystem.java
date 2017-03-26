b8513174b50f23fa71c28bba61480abddfdf0c26
Hai Le Tan <tanhai.infotech@gmail.com>
2017-03-26 11:36:21 +0700

    Refactor OS class

--- src/models/OperatingSystem.java
+++ src/models/OperatingSystem.java
@@ -1,23 +1,41 @@
 package models;
 
 public class OperatingSystem {
-  private ProcessesManagement processes;
-  private RootFilesManagement rootFiles;
+  private String name;
+  private ProcessesManagement processesManagement;
+  private RootFilesManagement rootFilesManagement;
 
-  public ProcessesManagement getProcesses() {
-    return processes;
+  public String getName() {
+    return name;
   }
 
-  public void setProcesses(ProcessesManagement processes) {
-    this.processes = processes;
+  public void setName(String name) {
+    this.name = name;
   }
 
-  public RootFilesManagement getRootFiles() {
-    return rootFiles;
+  public ProcessesManagement getProcessesManagement() {
+    return processesManagement;
   }
 
-  public void setRootFiles(RootFilesManagement rootFiles) {
-    this.rootFiles = rootFiles;
+  public void setProcessesManagement(ProcessesManagement processesManagement) {
+    this.processesManagement = processesManagement;
   }
 
+  public RootFilesManagement getRootFilesManagement() {
+    return rootFilesManagement;
+  }
+
+  public void setRootFilesManagement(RootFilesManagement rootFilesManagement) {
+    this.rootFilesManagement = rootFilesManagement;
+  }
+
+  public OperatingSystem() {
+    name = System.getProperty("os.name").toLowerCase();
+    if (name.indexOf("windows") > -1) {
+      processesManagement = (ProcessesManagement) new WindowProcesses();
+    } else {
+      processesManagement = (ProcessesManagement) new UnixProcesses();
+    }
+    rootFilesManagement = new RootFilesManagement();
+  }
 }
