7538f155d93032a6b5eeb0cff7a03990f963d49a
Hai Le Tan <tanhai.infotech@gmail.com>
2017-04-04 23:24:30 +0700

    Refactor Client class

--- src/network/Client.java
+++ src/network/Client.java
@@ -4,28 +4,14 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.SocketException;
 
 public class Client {
-  private DatagramSocket socket;
   private InetAddress ipAddress;
   private int port;
 
   public Client(InetAddress ipAddress, int port) {
     this.ipAddress = ipAddress;
     this.port = port;
-    try {
-      socket = new DatagramSocket();
-    } catch (SocketException e) {
-      e.printStackTrace();
-    }
-  }
-
-  public void close() {
-    if (socket == null || socket.isClosed()) {
-      return;
-    }
-    socket.close();
   }
 
   public InetAddress getIpAddress() {
@@ -36,34 +22,32 @@ public class Client {
     this.ipAddress = ipAddress;
   }
 
-  public String send(String message) {
-    byte[] data = message.getBytes();
-
-    // send string to ip address with port is 9876
-    DatagramPacket sendPacket = new DatagramPacket(data, data.length, getIpAddress(), getPort());
+  public String request(String message) {
+    String responseMessage = null;
     try {
+      byte[] data = message.getBytes();
+      DatagramSocket socket = new DatagramSocket();
+
+      // send string to ip address with port
+      DatagramPacket sendPacket = new DatagramPacket(data, data.length, getIpAddress(), getPort());
       socket.send(sendPacket);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
 
-    // receive response data from server
-    byte[] incomingData = new byte[getMaxBytes()];
-    DatagramPacket incomingPacket = new DatagramPacket(incomingData, incomingData.length);
-    try {
+      // receive response data from server
+      byte[] incomingData = new byte[getMaxBytes()];
+      DatagramPacket incomingPacket = new DatagramPacket(incomingData, incomingData.length);
       socket.receive(incomingPacket);
+      responseMessage = new String(incomingPacket.getData()).trim();
+      socket.close();
     } catch (IOException e) {
       e.printStackTrace();
     }
-
-    return new String(incomingPacket.getData());
+    return responseMessage;
   }
 
   public static void main(String[] args) {
     Client client = new Client(NetworkManagement.getInstance().getLocalAddress(), Server.getInstance().getPort());
-    System.out.println(client.send("Hey, this is the first time"));
-    System.out.println(client.send("demo"));
-    client.close();
+    System.out.println(client.request("Hey, this is the first time"));
+    System.out.println(client.request("demo"));
   }
 
   public int getMaxBytes() {
