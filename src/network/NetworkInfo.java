4c87bd38bff2a217041b1c9daadaa0778bb5bbbd
Hai Le Tan <tanhai.infotech@gmail.com>
2017-04-04 20:50:27 +0700

    Add white and black lists of Addresses

--- src/network/NetworkInfo.java
+++ src/network/NetworkInfo.java
@@ -4,6 +4,7 @@ import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Random;
 
@@ -11,10 +12,14 @@ public class NetworkInfo {
   private static int MAX_PORT = 65536;
   private static final String WLAN_NAME = "wlan0";
   private static NetworkInfo instance;
+  private ArrayList<InetAddress> whiteListAddresses;
+  private ArrayList<InetAddress> blackListAddresses;
   private int port;
 
   private NetworkInfo() {
     setPort();
+    whiteListAddresses = new ArrayList<>();
+    blackListAddresses = new ArrayList<>();
   }
 
   public static NetworkInfo getInstance() {
@@ -33,6 +38,40 @@ public class NetworkInfo {
 
   public static void main(String args[]) throws SocketException {
     System.out.println(NetworkInfo.getInstance().getLocalAddress().toString());
+  }
+
+  public void addToWhiteList(InetAddress whiteAddress) {
+    whiteListAddresses.add(whiteAddress);
+  }
+
+  public void addToBlackList(InetAddress blackAddress) {
+    blackListAddresses.add(blackAddress);
+  }
+
+  public boolean isWhiteAddress(InetAddress address) {
+    for (InetAddress inetAddress : whiteListAddresses) {
+      if (address.equals(inetAddress)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public boolean isBlackAddress(InetAddress address) {
+    for (InetAddress inetAddress : blackListAddresses) {
+      if (inetAddress.equals(address)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public void removeWhiteAddress(InetAddress address) {
+    whiteListAddresses.remove(address);
+  }
+
+  public void removeBlackAddress(InetAddress address) {
+    blackListAddresses.remove(address);
   }
 
   public InetAddress getLocalAddress() {
