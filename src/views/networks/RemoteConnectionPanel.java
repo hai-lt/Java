1be1dd264f53aa8e3142de3a4195d3754f6bc424
Hai Le Tan <tanhai.infotech@gmail.com>
2017-04-06 22:43:08 +0700

    Define Remote authentication panel

---
+++ src/views/networks/RemoteConnectionPanel.java
@@ -0,0 +1,56 @@
+package views.networks;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class RemoteConnectionPanel extends JPanel {
+  private JTextField txtIp, txtPassword;
+
+  public RemoteConnectionPanel() {
+    super();
+    setUi();
+  }
+
+  private void setUi() {
+    setBorder(BorderFactory.createTitledBorder("Remote Control"));
+    setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+    // set edit ip address
+    JPanel ipPanel = new JPanel();
+    ipPanel.setBorder(BorderFactory.createTitledBorder("Enter remote address"));
+    txtIp = new JTextField(15);
+    ipPanel.add(txtIp);
+    add(ipPanel);
+
+    // set edit password
+
+    JPanel passwordPanel = new JPanel();
+    passwordPanel.setBorder(BorderFactory.createTitledBorder("Password"));
+    txtPassword = new JTextField(15);
+    passwordPanel.add(txtPassword);
+    txtPassword.setBounds(0, 0, 150, 10);
+    add(passwordPanel);
+
+    // add button to connect
+    JButton btConnect = new JButton("Connect");
+    btConnect.addActionListener(connectRemote());
+    add(btConnect);
+
+  }
+
+  private ActionListener connectRemote() {
+    return new ActionListener() {
+
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        System.out.println(txtIp.getText() + " -- " + txtPassword.getText());
+      }
+    };
+  }
+}
