a221922d6a9806845ba0d922c3c7ae80783a4c01
Hai Le Tan <tanhai.infotech@gmail.com>
2017-04-07 23:32:35 +0700

    Refactor Main class

--- src/views/main/Main.java
+++ src/views/main/Main.java
@@ -3,7 +3,6 @@ package views.main;
 import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
 import javax.swing.JPanel;
-import javax.swing.JComponent;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
@@ -13,55 +12,56 @@ import views.networks.NetworkPanel;
 import views.process.ProcessesPanel;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
+import java.awt.Component;
 import java.awt.GridLayout;
 import java.awt.event.KeyEvent;
 
 public class Main extends JPanel {
-  private static String appName = "System Management";
+  private final static String NAME_TITLE = "System Management";
   private final static String PROCESSES_LABEL = "Processes";
   private final static String REMOTE_LABEL = "Access Remote";
   private final static String MEMORIES_LABEL = "Memories";
-  private final static int WIDTH = 500;
-  private final static int HEIGHT = 500;
+  private JTabbedPane tabMenu;
+  private OperatingSystem os;
 
-  public Main() {
+  public Main(OperatingSystem operatingSystem) {
     super(new GridLayout(1, 1));
 
-    JTabbedPane tabMenu = new JTabbedPane();
-    OperatingSystem os = new OperatingSystem();
-    ProcessesPanel pn = new ProcessesPanel(os.getProcessesManagement());
-    tabMenu.addTab(PROCESSES_LABEL, pn);
-    tabMenu.setMnemonicAt(0, KeyEvent.VK_P);
-
-    JComponent memoriesPanel = new MemoriesPanel(os.getRootFilesManagement());
-    tabMenu.addTab(MEMORIES_LABEL, memoriesPanel);
-    tabMenu.setMnemonicAt(1, KeyEvent.VK_M);
-
-    JComponent remotePanel = new NetworkPanel();
-    tabMenu.addTab(REMOTE_LABEL, remotePanel);
-    tabMenu.setMnemonicAt(2, KeyEvent.VK_A);
-    remotePanel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-
-    add(tabMenu);
-
+    os = operatingSystem;
+    tabMenu = new JTabbedPane();
     tabMenu.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+    add(tabMenu);
+    
+    addTab(new ProcessesPanel(os.getProcessesManagement()), PROCESSES_LABEL, KeyEvent.VK_P);
+    addTab(new MemoriesPanel(os.getRootFilesManagement()), MEMORIES_LABEL, KeyEvent.VK_M);
+    addTab(new NetworkPanel(), REMOTE_LABEL, KeyEvent.VK_A);
+
+  }
+
+  public void addTab(Component component, String tabName, int mnemonic) {
+    tabMenu.add(tabName, component);
+    tabMenu.setMnemonicAt(tabMenu.getTabCount() - 1, mnemonic);
+  }
+  
+  public String getTitle() {
+    return NAME_TITLE + " - " + os.getName().toUpperCase();
   }
 
   /**
    * Create the GUI and show it. For thread safety, this method should be
    * invoked from the event dispatch thread.
    */
-  private static void createAndShowGUI() {
+  private static void createAndShowGUI(Main ui) {
     // Create and set up the window.
-    JFrame frame = new JFrame(appName);
+    JFrame frame = new JFrame(ui.getTitle());
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     // Add content to the window.
-    frame.add(new Main(), BorderLayout.CENTER);
+    frame.add(ui, BorderLayout.CENTER);
 
     // Display the window.
     frame.pack();
+    frame.setResizable(false);
     frame.setVisible(true);
   }
 
@@ -72,7 +72,7 @@ public class Main extends JPanel {
       public void run() {
         // Turn off metal's use of bold fonts
         UIManager.put("swing.boldMetal", Boolean.FALSE);
-        createAndShowGUI();
+        createAndShowGUI(new Main(new OperatingSystem()));
       }
     });
   }
