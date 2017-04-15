82bdcdc0cdeeda8f20cd697c39c859bd2d1fb312
Hai Le Tan <tanhai.infotech@gmail.com>
2017-04-15 23:37:08 +0700

    Define processes api

--- src/network/Routes.java
+++ src/network/Routes.java
@@ -3,6 +3,7 @@ package network;
 import java.net.DatagramPacket;
 
 import controllers.DemoController;
+import controllers.ProcessesController;
 
 public class Routes {
   private static final String ROUTE_NOT_MATCH = "404 Not Found";
@@ -12,6 +13,9 @@ public class Routes {
     switch (receiveMessage) {
     case "demo":
       return DemoController.demo();
+    }
+    if (receiveMessage.indexOf(ProcessesController.RESOURCES) == 0) {
+      return new ProcessesController(receiveMessage).solve();
     }
     return ROUTE_NOT_MATCH;
   }
