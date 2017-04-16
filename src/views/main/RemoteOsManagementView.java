f48827657a01ea12d77c0ecf8cc28323efde5f5d
Hai Le Tan <tanhai.infotech@gmail.com>
2017-04-16 20:26:07 +0700

    Define refreshAction

--- src/views/main/RemoteOsManagementView.java
+++ src/views/main/RemoteOsManagementView.java
@@ -3,19 +3,39 @@ package views.main;
 import javax.swing.JFrame;
 
 import models.os.OperatingSystem;
+import models.processes.ProcessesManagement;
+import models.processes.UnixProcesses;
+import models.processes.WindowProcesses;
+import network.Client;
 
 public class RemoteOsManagementView extends BaseOsView {
+  private Client client;
 
-  public RemoteOsManagementView(OperatingSystem operatingSystem) {
+  public RemoteOsManagementView(OperatingSystem operatingSystem, Client client) {
     super(operatingSystem);
+    this.client = client;
   }
-  
-  public RemoteOsManagementView(String osString) {
+
+  public RemoteOsManagementView(String osString, Client client) {
     super(OperatingSystem.convertFrom(osString));
+    this.client = client;
   }
 
   @Override
   public JFrame create() {
-    return createUi(new RemoteOsManagementView(getOperatingSystem()));
+    return createUi(new RemoteOsManagementView(getOperatingSystem(), client));
+  }
+
+  @Override
+  public void refreshProcessesAction() {
+    String response = client.request("/processes");
+    ProcessesManagement processes;
+    if (getOperatingSystem().isWindowsOs()) {
+      processes = WindowProcesses.convertFrom(response);
+    } else {
+      processes = UnixProcesses.convertFrom(response);
+    }
+    setProcesses(processes);
+    notifyProcessesChanged();
   }
 }
