64e2bff7227d1cfd1a9710f07e1da478a4cdf23b
Hai Le Tan <tanhai.infotech@gmail.com>
2017-04-19 23:39:12 +0700

    Remove unuse method

--- src/views/networks/NetworkPanel.java
+++ src/views/networks/NetworkPanel.java
@@ -1,17 +1,12 @@
 package views.networks;
 
 import java.awt.BorderLayout;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import models.os.OperatingSystem;
-import network.Client;
 import network.RestfulRequest;
-import network.Server;
 
 public class NetworkPanel extends JPanel {
   private ConnectedAddressPanel connectedAddressPanel;
@@ -22,8 +17,6 @@ public class NetworkPanel extends JPanel {
     JScrollPane scrollPane = new JScrollPane(container);
     scrollPane.getVerticalScrollBar().setUnitIncrement(15);
     add(scrollPane, BorderLayout.CENTER);
-
-    ArrayList<Client> connected = getConnectedClient();
 
     RemoteConnectionPanel remoteConnectionPanel = new RemoteConnectionPanel() {
 
@@ -38,18 +31,10 @@ public class NetworkPanel extends JPanel {
     serverPanel.add(remoteConnectionPanel, BorderLayout.EAST);
     container.add(serverPanel, BorderLayout.NORTH);
 
-    connectedAddressPanel = new ConnectedAddressPanel(connected, 2);
+    connectedAddressPanel = new ConnectedAddressPanel(new ArrayList<>(), 2);
     connectedAddressPanel.setBorder(BorderFactory.createTitledBorder("Connected Server"));
     connectedAddressPanel.setEmptyElementView(new JLabel("Has no server connected!"));
     container.add(connectedAddressPanel, BorderLayout.CENTER);
-  }
-
-  private ArrayList<Client> getConnectedClient() {
-    ArrayList<Client> connected = new ArrayList<>();
-    for (int i = 0; i < 3; i++) {
-      connected.add(new Client(Server.getInstance().getAdress(), Server.getInstance().getPort()));
-    }
-    return connected;
   }
 
   private void connect(RestfulRequest request) {
@@ -60,6 +45,5 @@ public class NetworkPanel extends JPanel {
     }
     connectedAddressPanel.add(request);
     connectedAddressPanel.notifyDataHasChanged();
-    System.out.println("np 63: " +  response);
   }
 }
