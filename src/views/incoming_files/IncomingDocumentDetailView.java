8f004ccfb42c62b289d279ff772c6a508321304e
Hai Le Tan <tanhai.infotech@gmail.com>
2017-05-02 18:42:20 +0700

    Fix documentdetailview

--- src/views/incoming_files/IncomingDocumentDetailView.java
+++ src/views/incoming_files/IncomingDocumentDetailView.java
@@ -1,12 +1,16 @@
 package views.incoming_files;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import models.documents.DocumentRecord;
-import models.users.User;
 import models.users.UserRecord;
 import views.AppResources;
 import views.base.ImageHandle;
@@ -14,34 +18,100 @@ import views.base.ImageHandle;
 public class IncomingDocumentDetailView extends JPanel {
   private DocumentRecord document;
 
+  private JLabel sender, receiver, subject;
+  private JTextArea content;
+  private JTextField path;
+  private JButton back, gotoSrc;
+  private JPanel userInfo;
+
   public IncomingDocumentDetailView(DocumentRecord document) {
-    super();
+    super(new BorderLayout());
+    initializeView();
     this.document = document;
+    addData();
+    setUI();
+  }
+
+  public void initializeView() {
+    sender = new JLabel();
+    receiver = new JLabel();
+    userInfo = new JPanel(new BorderLayout(20, 10));
+    userInfo.add(sender, BorderLayout.WEST);
+    userInfo.add(receiver, BorderLayout.EAST);
+
+    subject = new JLabel();
+    content = new JTextArea(5, 15);
+    path = new JTextField();
+    back = new JButton("Back");
+    back.addActionListener(new ActionListener() {
+
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        backAction();
+      }
+    });
+    gotoSrc = new JButton(new ImageHandle(AppResources.FILE_ATTACHMENT_ICON).getIcon());
+    gotoSrc.addActionListener(new ActionListener() {
+
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        System.out.println("Open dialog");
+      }
+    });
+  }
+
+  public void addData() {
+    sender.setText("Người gửi: " + document.sender().getFullName());
+    String usersName = "";
+    for (UserRecord user : document.receivers()) {
+      usersName += user.getFullName() + ", ";
+    }
+    receiver.setText("Người nhận: " + usersName);
+    subject.setText("Chủ đề: " + document.getSubject());
+    content.setText(document.getContent());
+    path.setText(document.getSrc());
+  }
+
+  public void setUI() {
+    JPanel north = new JPanel(new BorderLayout());
     // show user info
-    JPanel userView = new JPanel();
-    UserRecord sender = new UserRecord(new User().all("code = " + document.getCode()).get(0));
-    userView.add(new JLabel("Người gửi: " + sender.getFullName()));
-    userView.add(new JLabel("Người nhận: " + "revievertest"));
-    add(userView);
+    north.add(userInfo, BorderLayout.NORTH);
 
     // add subject view
-    JPanel subjectView = new JPanel();
-    subjectView.add(new JLabel("Chủ đề: "));
-    subjectView.add(new JLabel("Hoc bong"));
-    add(subjectView);
+    JPanel subjectView = new JPanel(new BorderLayout());
+    subjectView.add(subject, BorderLayout.NORTH);
+    north.add(subjectView, BorderLayout.CENTER);
+    add(north, BorderLayout.NORTH);
 
+    JPanel center = new JPanel(new BorderLayout());
     // add content view
-    JPanel contentView = new JPanel();
-    contentView.add(new JLabel("Nội dung: "));
-    contentView.add(new JTextArea(document.getContent(), 5, 30));
-    add(contentView);
+    JPanel contentView = new JPanel(new BorderLayout());
+    contentView.add(new JLabel("Nội dung: "), BorderLayout.WEST);
+    contentView.add(content, BorderLayout.EAST);
+    center.add(contentView, BorderLayout.NORTH);
 
     // add attachment view
     JPanel attachmentView = new JPanel();
-    attachmentView.add(new JLabel(new ImageHandle(AppResources.FILE_ATTACHMENT_ICON).getIcon()));
-    JTextField pathView = new JTextField(document.getSrc());
-    pathView.setEnabled(false);
-    attachmentView.add(pathView);
-    add(attachmentView);
+    attachmentView.add(gotoSrc);
+    path.setEnabled(false);
+    attachmentView.add(path);
+    center.add(attachmentView, BorderLayout.CENTER);
+    add(center, BorderLayout.CENTER);
+  }
+
+  public DocumentRecord getDocument() {
+    return document;
+  }
+
+  public void setDocument(DocumentRecord document) {
+    this.document = document;
+  }
+
+  public void notifyDataHasChanged() {
+    addData();
+  }
+
+  public void backAction() {
+    setVisible(false);
   }
 }
--- src/views/incoming_files/IncommingDocumentManagementView.java
+++ src/views/incoming_files/IncommingDocumentManagementView.java
@@ -2,10 +2,13 @@ package views.incoming_files;
 
 import javax.swing.JPanel;
 
+import models.documents.Document;
+import models.documents.DocumentRecord;
+
 public class IncommingDocumentManagementView extends JPanel {
 
   public IncommingDocumentManagementView() {
     super();
-    add(new ListDocumentView());
+    add(new IncomingDocumentDetailView(new DocumentRecord(new Document().all().get(0))));
   }
 }
