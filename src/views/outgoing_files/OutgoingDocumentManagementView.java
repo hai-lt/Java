247eda9525893eed8f35dd83f6db8c939c2c1a65
Hai Le Tan <tanhai.infotech@gmail.com>
2017-05-03 21:31:57 +0700

    Add view to out going documents view

--- src/views/outgoing_files/OutgoingDocumentManagementView.java
+++ src/views/outgoing_files/OutgoingDocumentManagementView.java
@@ -1,12 +1,54 @@
 package views.outgoing_files;
 
-import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JPanel;
 
 public class OutgoingDocumentManagementView extends JPanel {
+  private NewDocumentView newDocumentView;
+  private OutgoingDocumentsView outgoingDocumentsView;
+
   public OutgoingDocumentManagementView() {
     super();
-    add(new JLabel("Văn bản đi"));
+    outgoingDocumentsView = new OutgoingDocumentsView();
+    outgoingDocumentsView.getBtnNew().addActionListener(newAction());
+    add(outgoingDocumentsView);
   }
 
+  private void showOutgoingDocumentsView() {
+    outgoingDocumentsView.setVisible(true);
+    newDocumentView.setVisible(false);
+  }
+
+  private void showNewDocumentView() {
+    outgoingDocumentsView.setVisible(false);
+    if (newDocumentView == null) {
+      newDocumentView = new NewDocumentView() {
+
+        @Override
+        protected void afterSaveAction() {
+          showOutgoingDocumentsView();
+        }
+
+        @Override
+        protected void afterBackAction() {
+          showOutgoingDocumentsView();
+        }
+      };
+      add(newDocumentView, BorderLayout.NORTH);
+    }
+    newDocumentView.setVisible(true);
+  }
+
+  private ActionListener newAction() {
+    return new ActionListener() {
+
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        showNewDocumentView();
+      }
+    };
+  }
 }
