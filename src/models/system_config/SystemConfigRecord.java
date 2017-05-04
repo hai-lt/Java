fda964054e7dd58cb991e96970dc6a555454c4ce
Hai Le Tan <tanhai.infotech@gmail.com>
2017-05-05 06:19:39 +0700

    Make systemconfig into singleton

--- src/models/system_config/SystemConfigRecord.java
+++ src/models/system_config/SystemConfigRecord.java
@@ -1,12 +1,42 @@
 package models.system_config;
 
+import java.sql.SQLException;
+import java.util.HashMap;
+
 import hailt.models.ObjectRecord;
 
 public class SystemConfigRecord {
   private String rememberUser;
+  private static SystemConfigRecord instance;
 
-  public SystemConfigRecord(ObjectRecord object) {
-    setAttributes(object.getValues());
+  public static SystemConfigRecord getInstance() {
+    if (instance != null) {
+      return instance;
+    }
+    synchronized (SystemConfigRecord.class) {
+      if (instance != null) {
+        return instance;
+      }
+      instance = new SystemConfigRecord();
+      return instance;
+    }
+  }
+
+  private SystemConfigRecord() {
+    try {
+      setAttributes(new SystemConfig().all().get(0).getValues());
+    } catch (Exception e) {
+      initializeSystemConfig();
+    }
+  }
+
+  private void initializeSystemConfig() {
+    try {
+      new SystemConfig().create(new HashMap<>());
+      setAttributes(new SystemConfig().all().get(0).getValues());
+    } catch (SQLException e1) {
+      System.out.println(e1.getMessage());
+    }
   }
 
   public void setAttributes(String... strings) {
