a8fc98e86f52ea905b7ba95012d76387edd75ae0
Hai Le Tan <tanhai.infotech@gmail.com>
2017-05-23 21:33:19 +0700

    Added getting processes info

--- src/models/processes/WindowProcess.java
+++ src/models/processes/WindowProcess.java
@@ -4,12 +4,12 @@ import java.util.Vector;
 
 public class WindowProcess extends ProcessInfo {
   public final static String[] TITLES = { "User", "Status", "Name", "Pid", "%Cpu", "Mem" };
-  private static final int POSITION_USER = 0;
-  private static final int POSITION_STATUS = 1;
-  private static final int POSITION_PID = 2;
-  private static final int POSITION_MEM = 3;
-  private static final int POSITION_CPU = 4;
-  private static final int POSITION_NAME = 5;
+  private static final int POSITION_USER = 2;
+  private static final int POSITION_STATUS = 3;
+  private static final int POSITION_PID = 1;
+  private static final int POSITION_MEM = 4;
+//  private static final int POSITION_CPU = 4;
+  private static final int POSITION_NAME = 0;
   private final String KILL_PROCESS_COMMAND = "taskkill ";
 
   @Override
@@ -27,13 +27,13 @@ public class WindowProcess extends ProcessInfo {
     String status = values[POSITION_STATUS];
     String name = values[POSITION_NAME];
     long id = Long.parseLong(values[POSITION_PID]);
-    float cpu = Float.parseFloat(values[POSITION_CPU]);
+//    float cpu = Float.parseFloat(values[POSITION_CPU]);
     float mem = Float.parseFloat(values[POSITION_MEM]);
-    setAttributes(user, status, name, id, cpu, mem);
+    setAttributes(user, status, name, id, 0, mem);
   }
 
-  public WindowProcess(String user, String status, String name, long id, float cpu, float mem) {
-    super(user, status, name, id, cpu, mem);
+  public WindowProcess(String user, String status, String name, long id, float mem) {
+    super(user, status, name, id, 0, mem);
   }
 
   @Override
